4d75cde49f46e8d4d5333816a89209e6a24dcbee
Yogesh Naik <yogeshrnaik@gmail.com>
2018-07-05 10:49:59 +0530

    initial commit

---
+++ docker/README.md
@@ -0,0 +1,5 @@
+# Docker
+**The Docker flow: Images to containers**
+
+ - An image is a file that makes up just enough of the operating system to do what you need to do.
+ - 
---
+++ docker/linkedin/Ex_Files_Docker_TheBasics/Exercise Files/reformat.sh
@@ -0,0 +1,1 @@
+export FORMAT="\nID\t{{.ID}}\nIMAGE\t{{.Image}}\nCOMMAND\t{{.Command}}\nCREATED\t{{.RunningFor}}\nSTATUS\t{{.Status}}\nPORTS\t{{.Ports}}\nNAMES\t{{.Names}}\n"
