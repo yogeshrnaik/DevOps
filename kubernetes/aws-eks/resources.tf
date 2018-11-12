03ee2639a768492428dff46aa4734907dd008e48
Yogesh Naik <yogeshrnaik@gmail.com>
2018-11-12 20:54:15 +0530

    after destroying

--- kubernetes/aws-eks/resources.tf
+++ kubernetes/aws-eks/resources.tf
@@ -13,15 +13,6 @@ module "eks" {
   subnets 		= ["subnet-c11f05b6", "subnet-a850bce0", "subnet-8813e1ec", "subnet-a3685afa"]
   vpc_id 		= "vpc-3842495d"
   
-  /*
-  workers_group_defaults = {
-    asg_desired_capacity          = "2"                             # Desired worker capacity in the autoscaling group.
-    asg_max_size                  = "3"                             # Maximum worker capacity in the autoscaling group.
-    asg_min_size                  = "1"                             # Minimum worker capacity in the autoscaling group.
-    instance_type                 = "t2.medium"                     # Size of the workers instances.
-  }
-  */
-  
   worker_groups         = [{
                             "asg_desired_capacity" = "3",
                             "asg_max_size" = "4",
@@ -32,17 +23,19 @@ module "eks" {
                             "asg_desired_capacity" = "2",
                             "asg_max_size" = "3",
                             "asg_min_size" = "1",
-							              "instance_type"  = "t2.nano"
+							              "instance_type"  = "t2.large"
                           }
                         ]
 }
 
 /*
+# Example:
+
 module "eks" {
   source                = "terraform-aws-modules/eks/aws"
   version               = "1.6.0"
   cluster_name          = "eks-dev-cluster"
-  subnets               = ["subnet-PUBLIC", "subnet-PRIVATE"]
+  subnets               = ["subnet-PUBLIC", "subnet-PRIVATE"] # Private Subnet must have NAT gateway otherwise nodes won't join EKS cluster
   tags                  = "${map("Environment", "Dev")}"
   vpc_id                = "vpc-VPC-ID"
   worker_groups         = [{
